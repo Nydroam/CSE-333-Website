b52d1dd6d0ab6edd5ffa29740314438cfbade6bb
Raymond Wu <rayw.u17@gmail.com>
2019-11-22 19:05:20 -0500

    Updates

---
+++ precache-manifest.2ab85654ac397a9f14f54591ad966ca9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ea4eb9db5c871d03e5809132d3f852",
+    "url": "/sbu-tabletop-website/index.html"
+  },
+  {
+    "revision": "2849dd018bf3f2b4a510",
+    "url": "/sbu-tabletop-website/static/css/main.370ea069.chunk.css"
+  },
+  {
+    "revision": "d8cf9b9ff35cda4b8f7b",
+    "url": "/sbu-tabletop-website/static/js/2.ee0e1df4.chunk.js"
+  },
+  {
+    "revision": "2849dd018bf3f2b4a510",
+    "url": "/sbu-tabletop-website/static/js/main.8be8e6ed.chunk.js"
+  },
+  {
+    "revision": "d91ef1c0198291fdde4e",
+    "url": "/sbu-tabletop-website/static/js/runtime-main.640440ab.js"
+  },
+  {
+    "revision": "f23601c6b2d647e7d3635c2a4862a50e",
+    "url": "/sbu-tabletop-website/static/media/About.f23601c6.png"
+  },
+  {
+    "revision": "e76840735e6ac4446992f22b3c6c41c8",
+    "url": "/sbu-tabletop-website/static/media/Dice.e7684073.png"
+  },
+  {
+    "revision": "cb125877edce66887e4e20644cd84bd7",
+    "url": "/sbu-tabletop-website/static/media/Effra_Std_Lt.cb125877.ttf"
+  },
+  {
+    "revision": "c126c6c0c439a1e85936921fd1c430d3",
+    "url": "/sbu-tabletop-website/static/media/Effra_Std_Md.c126c6c0.ttf"
+  },
+  {
+    "revision": "2556d7a29ab01dc80b96eb824a049df6",
+    "url": "/sbu-tabletop-website/static/media/Effra_Std_Rg.2556d7a2.ttf"
+  },
+  {
+    "revision": "728b0f9e71a224a059a7457842b39c03",
+    "url": "/sbu-tabletop-website/static/media/Events.728b0f9e.png"
+  },
+  {
+    "revision": "a925971d2dc9cbb8d607524ed8cac6e8",
+    "url": "/sbu-tabletop-website/static/media/Games.a925971d.png"
+  },
+  {
+    "revision": "86cadc92858682d362ade77e0ca37c74",
+    "url": "/sbu-tabletop-website/static/media/MrsEavesRoman.86cadc92.ttf"
+  },
+  {
+    "revision": "a75cfe73c45e5208cd26b9998d7a139b",
+    "url": "/sbu-tabletop-website/static/media/catan.a75cfe73.jpeg"
+  },
+  {
+    "revision": "6649584da796f0dcbbed647f33471097",
+    "url": "/sbu-tabletop-website/static/media/exploding.6649584d.png"
+  },
+  {
+    "revision": "8c019978bbf5b641fe3662d3554eb34d",
+    "url": "/sbu-tabletop-website/static/media/game_event.8c019978.jpg"
+  },
+  {
+    "revision": "c8237236551b7add227bf508a05f0c0c",
+    "url": "/sbu-tabletop-website/static/media/original_logo.c8237236.jpg"
+  },
+  {
+    "revision": "23e39d08922e5d6c1b2f4c7d98a0f5f8",
+    "url": "/sbu-tabletop-website/static/media/phb.23e39d08.png"
+  },
+  {
+    "revision": "7781d3701dd34f770b27e655fcc93a12",
+    "url": "/sbu-tabletop-website/static/media/tabletoplogo_colored.7781d370.png"
+  },
+  {
+    "revision": "8b90d6671d9a96f02df0b30b1d8bbb5b",
+    "url": "/sbu-tabletop-website/static/media/typography.8b90d667.png"
+  }
+]);
